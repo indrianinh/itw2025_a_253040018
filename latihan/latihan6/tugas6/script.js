d5aef23046e1a16d3a153075f89f9d4c16f03573
indrianinh <indrianinurhidayah212@gmail.com>
2025-12-01 21:11:42 +0700

    selesai mengerjakan tugas6

---
+++ latihan/latihan6/tugas6/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector ('.menu-toggle input')
+const nav = document.querySelector('nav ul')
+
+menuToggle.addEventListener('click'.function() {
+    nav.classlist.toggle('slide')
+})
